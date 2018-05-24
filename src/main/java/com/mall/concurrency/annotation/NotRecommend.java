b56f391aa1f80a871e2e8db596e40d7033e093c2
Jefflb <jefflb007@hotmail.com>
2018-05-24 23:34:26 +0800

    added anotation class

---
+++ src/main/java/com/mall/concurrency/annotation/NotRecommend.java
@@ -0,0 +1,15 @@
+package com.mall.concurrency.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 课程用来标记【不推荐】的写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotRecommend {
+    String value() default "";
+}
---
+++ src/main/java/com/mall/concurrency/annotation/Recommend.java
@@ -0,0 +1,15 @@
+package com.mall.concurrency.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 课程用来标记【推荐】的写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Recommend {
+    String value() default "";
+}
